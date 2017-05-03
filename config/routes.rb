21d77e74e0d4e07a51addf2832b10aebfefce90d
Miguel Angel Caro <mcaro@aspgems.com>
2017-05-03 20:09:25 +0200

    First version of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'sessions/new'
   get 'home/index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
