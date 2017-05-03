37328d6e334c6927c0a5ea07a92b5898ffbf1826
Miguel Angel Caro <mcaro@aspgems.com>
2017-05-03 20:34:00 +0200

    Added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
   get 'home/index'
+  get  '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
